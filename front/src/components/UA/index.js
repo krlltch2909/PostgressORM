0f950572fda59c226f7a2c7eb73dce02fccf7ea7
AipNooBest <47401054+AipNooBest@users.noreply.github.com>
2022-11-17 21:12:42 +0300

    ???

--- front/src/components/UA/index.js
+++ front/src/components/UA/index.js
@@ -1,11 +1,11 @@
 import MyInput from "@/components/UA/MyInput";
 import TaskItem from "@/components/UA/TaskItem";
 import MyNavBar from "@/components/UA/MyNavBar";
-import MyDiolog from "@/components/UA/MyDiolog";
+import MyDialog from "@/components/UA/MyDialog";
 import ContractItem from "@/components/UA/ContractItem";
 import MySelect from "@/components/UA/MySelect";
 
 
 export default [
-    MyInput, TaskItem, MyNavBar, MyDiolog,  ContractItem, MySelect
+    MyInput, TaskItem, MyNavBar, MyDialog,  ContractItem, MySelect
 ]
